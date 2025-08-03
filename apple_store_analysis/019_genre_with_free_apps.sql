96713afcd59bed3e24b1ce6ff6602f3588251ee5
Elham Shahrokhi <ellishahi95@gmail.com>
2025-08-03 14:37:04 +0200

    finding genres with more than 50% free apps

---
+++ apple_store_analysis/019_genre_with_free_apps.sql
@@ -0,0 +1,7 @@
+SELECT
+  prime_genre,
+  COUNT(*) AS total_apps,
+  SUM(CASE WHEN price = 0 THEN 1 ELSE 0 END) AS free_apps
+FROM apple_store
+GROUP BY prime_genre
+HAVING SUM(CASE WHEN price = 0 THEN 1 ELSE 0 END) > 0.5 * COU(*);
