f4e754c9565f75413f991aace9d44945999f710d
Elham Shahrokhi <ellishahi95@gmail.com>
2025-08-03 12:22:32 +0200

    price types of apps

---
+++ apple_store_analysis/005_price_type_apps.sql
@@ -0,0 +1,10 @@
+SELECT
+    CASE
+        WHEN price = 0 THEN 'free'
+        WHEN price BETWEEN 0 AND 5 THEN 'cheap'
+        WHEN price BETWEEN 5 AND 10 THEN 'mooderate'
+        ELSE 'expensive'
+    END AS app_type,
+    COUNT(*) AS total_apps
+FROM apple_store
+GROUP BY app_type;
