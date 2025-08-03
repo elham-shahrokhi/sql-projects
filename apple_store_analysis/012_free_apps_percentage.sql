855f69ddc7f0473ba4ca7bfffd2154fa9badae2d
Elham Shahrokhi <ellishahi95@gmail.com>
2025-08-03 12:53:39 +0200

    finding the percentage of free apps in each genre

---
+++ apple_store_analysis/012_free_apps_percentage.sql
@@ -0,0 +1,13 @@
+SELECT
+  prime_genre,
+  total_apps,
+  free_apps,
+  ROUND(free_apps * 100.0 / total_apps, 2) AS free_percent
+FROM(
+  SELECT
+    prime_genre,
+    COUNT(*) AS total_apps,
+    SUM(CASE WHEN price = 0 THEN 1 ELSE 0 END) AS free_apps
+  FROM apple_store
+  GROUP BY prime_genre
+) AS sub;
