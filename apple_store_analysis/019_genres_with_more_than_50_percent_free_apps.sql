599afa5d87beaf252c2eb1d43937da93ca95c29d
Elham Shahrokhi <ellishahi95@gmail.com>
2025-08-04 12:48:32 +0200

    filter genres where free apps exceed 50% of total apps

--- apple_store_analysis/019_genres_with_more_than_50_percent_free_apps.sql
+++ apple_store_analysis/019_genres_with_more_than_50_percent_free_apps.sql
@@ -4,4 +4,4 @@ SELECT
   SUM(CASE WHEN price = 0 THEN 1 ELSE 0 END) AS free_apps
 FROM apple_store
 GROUP BY prime_genre
-HAVING SUM(CASE WHEN price = 0 THEN 1 ELSE 0 END) > 0.5 * COU(*);
+HAVING SUM(CASE WHEN price = 0 THEN 1 ELSE 0 END) > 0.5 * COUNT(*);
