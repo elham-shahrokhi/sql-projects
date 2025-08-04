34e52f9a2db68a089414f06d806482d95755eaa8
Elham Shahrokhi <ellishahi95@gmail.com>
2025-08-04 12:24:05 +0200

    finding the number of apps based on price

--- apple_store_analysis/005_price_type_apps.sql
+++ apple_store_analysis/005_price_type_apps.sql
@@ -2,7 +2,7 @@ SELECT
     CASE
         WHEN price = 0 THEN 'free'
         WHEN price BETWEEN 0 AND 5 THEN 'cheap'
-        WHEN price BETWEEN 5 AND 10 THEN 'mooderate'
+        WHEN price BETWEEN 5 AND 10 THEN 'moderate'
         ELSE 'expensive'
     END AS app_type,
     COUNT(*) AS total_apps
